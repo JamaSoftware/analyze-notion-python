2490def35fd101c10b540935fd33e0e6ce0a3804
Nick Burns <nburns@users.noreply.github.com>
2018-03-07 13:52:26 -0800

    add links to setup

--- setup.py
+++ setup.py
@@ -3,7 +3,14 @@ setup(
     name='notion-python',
     version='1.0.0',
     description='API Client for Notion',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
     keywords='notion api client',
+    project_urls={
+        'Source': 'https://github.com/notion-data/notion-python',
+    },
     packages=find_packages(),
     install_requires=['requests'],
 )
